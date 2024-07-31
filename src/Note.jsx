eaff6141274505879813dbbdfa96fc8a5e4b1347
ManSteel007 <manan.jindal21@st.niituniversity.in>
2024-08-01 01:29:32 +0530

    Added mapping function for getting a series of notes.

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Note() {
+function Note(props) {
     return (
         <div className="note">
-            <h1>Title</h1>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
